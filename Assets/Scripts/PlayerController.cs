421dc83ec31d6e5887fbf2549cdf0e419465b2c6
NathaliePatzer <nathalielaise@gmail.com>
2025-03-19 21:54:58 -0300

    user inputs :wrench:

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    Vector2 movement;
+    Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime*speed);
+    }
+
+    public void Movement(InputAction.CallbackContext context) {
+        //down
+        if(context.performed) {
+            movement = context.ReadValue<Vector2>();
+        }
+        //up
+        else if(context.canceled) {
+            movement = Vector2.zero;
+        }
+    }
+
+    public void Shoot(InputAction.CallbackContext context) {
+        Debug.Log("Pew");
+    }
+}

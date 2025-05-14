726931a659d47a69ddf2bbfd1073726a8d61e00a
NathaliePatzer <nathalielaise@gmail.com>
2025-05-14 16:05:46 -0300

    add new sprites && transfer aliens movement detector :adhesive_bandage:

--- Assets/Scripts/MovementChangeDetector.cs
+++ Assets/Scripts/MovementChangeDetector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -5,9 +6,41 @@ using UnityEngine;
 
 public class MovementChangeDetector : MonoBehaviour
 {
-   void OnTriggerEnter2D(Collider2D coll) {
-        if(coll.transform.name.Contains("wall")) {
-            AlienController.Instance.direction *= -1;
+    public Vector2Int direction;
+    public Transform wall;
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Bullet bullet;
+        if (coll.transform == wall)
+        {
+            AlienController.Instance.direction = direction;
         }
-   }
+        if (coll.TryGetComponent<Bullet>(out bullet) && bullet.team == Team.Player)
+        {
+            WhenDisabled();
+        }
+
+    }
+    void WhenDisabled()
+    {
+        Alien alien = GetComponent<Alien>();
+        int y = alien.matrixPos.y + 1;
+
+        for (int x = alien.matrixPos.x; x > -1 && x < AlienController.Instance.aliens.GetLength(0); x += direction.x)
+        {
+            for (; y < AlienController.Instance.aliens.GetLength(1); y++)
+            {
+                if (AlienController.Instance.aliens[x, y] != null)
+                {
+                    MovementChangeDetector detector = AlienController.Instance.aliens[x, y].gameObject.AddComponent<MovementChangeDetector>();
+                    detector.direction = direction;
+                    detector.wall = wall;
+                    this.enabled = false;
+                    return;
+                }
+            }
+            y = 0;
+        }
+
+    }
 }

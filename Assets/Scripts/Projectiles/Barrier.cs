dfd859106b15c516f7d7e928fa1a021427e7d7cb
NathaliePatzer <nathalielaise@gmail.com>
2025-06-03 00:18:55 -0300

    special addition: damaged barriers :sparkles:

--- Assets/Scripts/Projectiles/Barrier.cs
+++ Assets/Scripts/Projectiles/Barrier.cs
@@ -4,14 +4,43 @@ using UnityEngine;
 
 public class Barrier : MonoBehaviour
 {
-    int lives = 5;
+    int lives = 15;
+    public Sprite[] damageSprites;
+    private SpriteRenderer spriteRenderer;
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateSprite();
+    }
 
-    void OnTriggerEnter2D(Collider2D coll) {
-        if (coll.GetComponent<Bullet>()) {
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.GetComponent<Bullet>())
+        {
             lives--;
-            if (lives <= 0) {
+            UpdateSprite();
+            
+            if (lives <= 0)
+            {
                 Destroy(this.gameObject);
             }
         }
     }
+    void UpdateSprite()
+    {
+        int index = 0;
+
+        if (lives <= 5)
+            index = 2;
+        else if (lives <= 10)
+            index = 1;
+        else
+            index = 0;
+
+        if (damageSprites != null && damageSprites.Length > index)
+        {
+            spriteRenderer.sprite = damageSprites[index];
+        }
+    }
+    
 }
